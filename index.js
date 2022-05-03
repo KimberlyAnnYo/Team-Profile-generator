35aac78f5b912ff41459ee84a889d365e15cd0dc
Kimberly West <kdenton993@gmail.com>
2022-05-02 18:34:24 -0600

    add email prompt

--- index.js
+++ index.js
@@ -29,7 +29,34 @@ const addmanager = () => {
                 }
             }
         },
-       
-
+        {
+            type: 'input',
+            name: 'id',
+            message:'enter the managers ID.',
+            validate: nameInput => {
+                if (isNaN(nameInput)){
+                    console.log('please enter rhe managers id')
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+// enter email prompt
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },
+        
     ])
 }
